efb6c16a00fe56d2bb6b628b1b11c0de3941d805
Saumya Verma <149528902+saumyaverma11@users.noreply.github.com>
2026-03-13 15:57:52 +0530

    Update Landing page.tsx

--- Code/TinyURLWeb/TinyURLWebsite/src/pages/LandingPage.tsx
+++ Code/TinyURLWeb/TinyURLWebsite/src/pages/LandingPage.tsx
@@ -45,7 +45,7 @@ export default function LandingPage() {
 
             <Link
               to="/register"
-              className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-indigo-700 transition"
+              className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
             >
               Get Started Free
             </Link>
